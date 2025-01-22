2ed0b659c9fa4b8fc5550ea14c9538d587d17e44
HerminiaAlv <158019155+HerminiaAlv@users.noreply.github.com>
2025-01-22 14:59:41 -0800

    Thermostat.java

---
+++ Thermostat.java
@@ -0,0 +1,9 @@
+public class Thermostat {
+    public void setTemperature(int temperature) {
+        System.out.println("Thermostat set to " + temperature + "°C.");
+    }
+
+    public void turnOff() {
+        System.out.println("Thermostat is OFF.");
+    }
+}
